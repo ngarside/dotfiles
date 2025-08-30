4d94cc20a359f42af5a81ac0bcfa45c276d1b703
Nathan Garside <git@nathangarside.com>
2025-08-30 10:19:29 +0100

    Add license header

--- home/dot_bashrc.d/defaults.sh
+++ home/dot_bashrc.d/defaults.sh
@@ -1,10 +1,12 @@
-# Make auto-complete ignore case
+# This is free and unencumbered software released into the public domain.
+
+# Make auto-complete ignore case.
 bind -s 'set completion-ignore-case on'
 
-# Disable systemd's default paging
+# Disable systemd's default paging.
 export SYSTEMD_PAGER=
 
-# Default to /home/nathan rather than /var/home/nathan
+# Default to '/home/nathan' rather than '/var/home/nathan'.
 if [ "$PWD" = "/var/home/nathan" ]; then
 	cd $HOME
 fi
